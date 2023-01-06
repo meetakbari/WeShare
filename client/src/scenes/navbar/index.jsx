4716542301e1682acddc25815118e5cabcb2a66d
meetakbari <meetakbari.me@gmail.com>
2023-01-06 23:13:14 +0530

    Authentication and authorization integration with frontend completed

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -39,8 +39,8 @@ const Navbar = () => {
     const primaryLight = theme.palette.primary.light;
     const alt = theme.palette.background.alt;
 
-    /*  const fullName = `${user.firstName} ${user.lastName}`; */
-    const fullName = `Meet Akbari`;
+    const fullName = `${user.firstName} ${user.lastName}`;
+    // const fullName = `Meet Akbari`;
 
     return (
         <FlexBetween padding="1rem 6%" backgroundColor={alt}>
